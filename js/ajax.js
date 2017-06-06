2e6085a298b438f21014ed9e72cfda2c1cd63d97
isliangj <isliangj@icloud.com>
2017-06-06 21:33:38 +0800

    new5

---
+++ js/ajax.js
@@ -0,0 +1,8 @@
+// $.ajax({
+//     method: "GET",
+//     url:"http://39.108.63.108/Sign_In/AdminTeacher/findAll.action?page=1",
+//   })
+//     .done(function(msg){
+//       console.log(msg);
+//     });
+
