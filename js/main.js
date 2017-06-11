82b8764debf18d1e59b3cfb515ab1218f24dd160
isliangj <isliangj@icloud.com>
2017-06-12 02:07:02 +0800

    BM 1.0
    完成增删改查

--- js/main.js
+++ js/main.js
@@ -178,22 +178,89 @@ function update(i){
 	$("select[name='aid']").val(Mymsg.rows[i].dbAcademy.aid);
 
 	$('#updateinput').click(function(){
-		
-	});
+		$.ajax({
+			url:"http://39.108.63.108/Sign_In/AdminStudent/update.action?" + $("#mainform").serialize() +"&sid="+Mymsg.rows[i].sid,
+			type:'GET',
+		});
+		$("tr").remove("#pag_li"+i);
 
+		var sname= $("input[name='sname']").val();
+		var snumber= $("input[name='snumber']").val();
+		var ssex= $("input[name='ssex']").val();
+		var sage= $("input[name='sage']").val();
+		var sdata= $("input[name='sdata']").val();
+		var cid= $("input[name='cid']").val();
+		var aid=$("select[name='aid']").find("option:selected").text();
+		Myi++;
 
-
-
-
-
-	// $.ajax({
-	// 	url:"http://39.108.63.108/Sign_In/AdminStudent/update.action?" + $("#mainform").serialize(),
-	// 	type:'GET',
-	// })
-	// 	.done (function(msg){
-
-	// 	});
-	// $("tr").remove(".pag_li");
-
-	// document.getElementById("mainform").reset();
+		var tTr="<tr class='pag_li' id='pag_li"+Myi+"'><td>"+sname+"</td><td>"+snumber+"</td><td>"+ssex+"</td><td>"+sage+"</td><td>"+aid+"</td><td class='content_button'><button onclick='del("+Myi+")'>删除</button><button class='btn btn-info btn-lg show-layer' data-show-layer='hw-layer'>修改</button></td></tr>";
+		$("#dataTbody").append(tTr);
+		document.getElementById("mainform").reset();
+		});
 }
+
+/*
+搜索
+*/
+$('#search_btn').click(function(){
+	var sel_sel=$("select[name='search_sel']").val();
+	var sel_inp= $("input[name='search']").val();
+
+	$.ajax({
+		url:"http://39.108.63.108/Sign_In/AdminStudent/select.action?" + sel_sel + "=" +sel_inp,
+		type:'GET',
+	})
+		.done(function(msg){
+			Mymsg=msg;
+    	$("tr").remove(".pag_li");
+			for(var i=0;i<msg.rows.length;i++)
+			{
+      	var tTr="<tr class='pag_li' id='pag_li"+i+"'><td>"+msg.rows[i].sname+"</td><td>"+msg.rows[i].snumber+"</td><td>"+msg.rows[i].ssex+"</td><td>"+msg.rows[i].sage+"</td><td>"+msg.rows[i].dbAcademy.aname+"</td><td class='content_button'><button onclick='del("+i+")'>删除</button><button class='btn btn-info btn-lg show-layer' data-show-layer='hw-layer'>修改</button></td></tr>";
+				$("#dataTbody").append(tTr);
+
+				$(function show(){
+						//展示层
+						function showLayer(id){
+							var layer = $('#'+id),
+								layerwrap = layer.find('.hw-layer-wrap');
+							layer.fadeIn();
+							//屏幕居中
+							layerwrap.css({
+								'margin-top': -layerwrap.outerHeight()/2
+							});
+						}
+
+						//隐藏层
+						function hideLayer(){
+							$('.hw-overlay').fadeOut();
+						}
+
+						$('.hwLayer-ok,.hwLayer-cancel,.hwLayer-close').on('click', function() {
+							hideLayer();
+						});
+
+						//触发弹出层
+						$('.show-layer').on('click',  function() {		
+							var layerid = $(this).data('show-layer');
+							showLayer(layerid);
+						});
+
+						//点击或者触控弹出层外的半透明遮罩层，关闭弹出层
+						$('.hw-overlay').on('click',  function(event) {
+							if (event.target == this){
+								hideLayer();
+							}
+						});
+
+						//按ESC键关闭弹出层
+						$(document).keyup(function(event) {
+							if (event.keyCode == 27) {
+								hideLayer();
+							}
+						});
+					});
+
+			}
+			Myi = i;
+		});
+	});
